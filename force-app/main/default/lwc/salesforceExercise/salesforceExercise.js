d81e124a85883d8a67991e9100e8a72ca80f6c03
AntonioLV1999 <antoniolebrerovitero@gmail.com>
2024-05-02 13:50:23 +0200

    Controllers and Minor Changes

---
+++ force-app/main/default/lwc/salesforceExercise/salesforceExercise.js
@@ -0,0 +1,89 @@
+import { LightningElement, track, wire } from 'lwc';
+import searchAirports from '@salesforce/apex/AirportController.searchAirports';
+import saveFlight from '@salesforce/apex/FlightController.saveFlight';
+
+export default class SalesforceExercise extends LightningElement {
+    @track departureIata;
+    @track arrivalIata;
+    @track departureAirports = [];
+    @track arrivalAirports = [];
+    @track flight;
+    @track error;
+
+    handleDepartureChange(event) {
+        this.departureIata = event.target.value;
+        this.searchDepartureAirports();
+    }
+
+    handleArrivalChange(event) {
+        this.arrivalIata = event.target.value;
+        this.searchArrivalAirports();
+    }
+
+    searchDepartureAirports() {
+        if(this.departureIata.length >= 3) {
+            searchAirports({ searchText: this.departureIata })
+                .then(result => {
+                    this.departureAirports = result;
+                    this.error = undefined;
+                })
+                .catch(error => {
+                    this.error = error;
+                    this.departureAirports = [];
+                });
+        } else {
+            this.departureAirports = [];
+        }
+    }
+
+    searchArrivalAirports() {
+        if(this.arrivalIata.length >= 3) {
+            searchAirports({ searchText: this.arrivalIata })
+                .then(result => {
+                    this.arrivalAirports = result;
+                    this.error = undefined;
+                })
+                .catch(error => {
+                    this.error = error;
+                    this.arrivalAirports = [];
+                });
+        } else {
+            this.arrivalAirports = [];
+        }
+    }
+
+    selectDepartureAirport(event) {
+        var clickDeparture = event.target.getAttribute("id");
+        var departure = clickDeparture.substring(0, 3);
+        console.log('Departure IATA: ' + departure);
+        this.departureIata = departure;
+        this.departureAirports = [];
+    }
+
+    selectArrivalAirport(event) {
+        var clickArrival = event.target.getAttribute("id");
+        var arrival = clickArrival.substring(0, 3);
+        console.log('Arrival IATA: '+arrival);
+        this.arrivalIata = arrival;
+        this.arrivalAirports = [];
+    }
+
+    handleSave(){
+        console.log('departures: '+this.departureIata);
+        if (this.departureIata && this.arrivalIata) {
+            saveFlight({ departureIata: this.departureIata, arrivalIata: this.arrivalIata })
+                .then(result => {
+                    this.flight = result;
+                    console.log('muy bien: ' + this.flight);
+                    this.error = undefined;
+                })
+                .catch(error => {
+                    this.error = error.message;
+                    console.log('puto pringao '+this.error);
+                    this.flight = undefined;
+                });
+        } else {
+            this.error = 'Please select both departure and arrival airports.';
+        }
+    }
+}
